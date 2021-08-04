e75795107e3a9dca75dcf3451064a0e9bf084e43
Brandon-Gui123 <BrandonGui123Alt@gmail.com>
2021-08-04 15:59:47 +0800

    Create namespace for game script

--- Assets/Flipping Tiles Game in Editor/Editor/MemoryTileGameWindow.cs
+++ Assets/Flipping Tiles Game in Editor/Editor/MemoryTileGameWindow.cs
@@ -6,72 +6,74 @@ using System.Collections;
 using System.Linq;
 using Unity.EditorCoroutines.Editor;
 
-public class MemoryTileGameWindow : EditorWindow
+namespace FlippingTiles
 {
-    private string[] colourStringsInUse =
+    public class MemoryTileGameWindow : EditorWindow
     {
+        private string[] colourStringsInUse =
+        {
         "red", "orange", "yellow", "green", "blue", "indigo", "magenta", "cyan", "dark-green"
     };
 
-    private Button previouslyOpenedTile;
-    private bool isOpeningAllowed = true;
-    private int unmatchedTilesLeft = 36;
+        private Button previouslyOpenedTile;
+        private bool isOpeningAllowed = true;
+        private int unmatchedTilesLeft = 36;
 
-    [MenuItem("MemoryTile/Open")]
-    public static void OpenMemoryTileGameWindow()
-    {
-        var window = GetWindow<MemoryTileGameWindow>();
-        window.titleContent = new GUIContent("Memory Tile");
-    }
-
-    private void OnEnable()
-    {
-        // creates a List containing 4 of each possible colours
-        var colourStrings = new List<string>();
-
-        foreach (var colourString in colourStringsInUse)
+        [MenuItem("MemoryTile/Open")]
+        public static void OpenMemoryTileGameWindow()
         {
-            for (int i = 0; i < 4; i++)
-            {
-                colourStrings.Add(colourString);
-            }
+            var window = GetWindow<MemoryTileGameWindow>();
+            window.titleContent = new GUIContent("Memory Tile");
         }
 
-        // shuffle list
-        var rand = new System.Random();
-        colourStrings = colourStrings.OrderBy(str => rand.Next()).ToList();
-
-        var gameWindowTreeAsset = Resources.Load<VisualTreeAsset>("MemoryTileGameWindow");
-        gameWindowTreeAsset.CloneTree(rootVisualElement);
-
-        for (int i = 0; i < 6; i++)
+        private void OnEnable()
         {
-            var tileBoxTreeAsset = Resources.Load<VisualTreeAsset>("TileBox");
-            tileBoxTreeAsset.CloneTree(rootVisualElement);
-        }
+            // creates a List containing 4 of each possible colours
+            var colourStrings = new List<string>();
 
-        var tileBoxes = rootVisualElement.Query<VisualElement>(className: "tile-box");
-        tileBoxes.ForEach(
-            ve =>
+            foreach (var colourString in colourStringsInUse)
             {
-                for (int i = 0; i < 6; i++)
+                for (int i = 0; i < 4; i++)
                 {
-                    var tileTreeAsset = Resources.Load<VisualTreeAsset>("Tile");
-                    tileTreeAsset.CloneTree(ve);
+                    colourStrings.Add(colourString);
                 }
             }
-        );
 
-        // all tile boxes will need to do the following
-        rootVisualElement.Query<Button>(name: "TileButton").ForEach(
-            btn =>
+            // shuffle list
+            var rand = new System.Random();
+            colourStrings = colourStrings.OrderBy(str => rand.Next()).ToList();
+
+            var gameWindowTreeAsset = Resources.Load<VisualTreeAsset>("MemoryTileGameWindow");
+            gameWindowTreeAsset.CloneTree(rootVisualElement);
+
+            for (int i = 0; i < 6; i++)
             {
+                var tileBoxTreeAsset = Resources.Load<VisualTreeAsset>("TileBox");
+                tileBoxTreeAsset.CloneTree(rootVisualElement);
+            }
+
+            var tileBoxes = rootVisualElement.Query<VisualElement>(className: "tile-box");
+            tileBoxes.ForEach(
+                ve =>
+                {
+                    for (int i = 0; i < 6; i++)
+                    {
+                        var tileTreeAsset = Resources.Load<VisualTreeAsset>("Tile");
+                        tileTreeAsset.CloneTree(ve);
+                    }
+                }
+            );
+
+            // all tile boxes will need to do the following
+            rootVisualElement.Query<Button>(name: "TileButton").ForEach(
+                btn =>
+                {
                 // indicates that it isn't open
                 btn.text = "?";
 
                 // randomly assigning a colour
                 btn.Q<VisualElement>(className: "tile-colour-visual").ToggleInClassList(colourStrings[0]);
-                btn.Q<VisualElement>(name: "TileColour").ToggleInClassList(colourStrings[0]);
+                    btn.Q<VisualElement>(name: "TileColour").ToggleInClassList(colourStrings[0]);
 
                 // so our next button can access the next colour string
                 colourStrings.RemoveAt(0);
@@ -81,96 +83,97 @@ public class MemoryTileGameWindow : EditorWindow
 
                 // every button needs to perform an action when clicked
                 btn.clicked += () => OnTileButtonClicked(btn);
-            }
-        );
-    }
-
-    private void OnTileButtonClicked(Button tileButton)
-    {
-        if (!isOpeningAllowed)
-        {
-            return;
+                }
+            );
         }
 
-        if (previouslyOpenedTile == tileButton)
+        private void OnTileButtonClicked(Button tileButton)
         {
-            // close the tile
-            var tileButtonVisual = tileButton.Q<VisualElement>(className: "tile-colour-visual");
-            tileButtonVisual.style.display = DisplayStyle.None;
-            previouslyOpenedTile = null;
-        }
-        else if (previouslyOpenedTile == null)
-        {
-            previouslyOpenedTile = tileButton;
-
-            // open the tile
-            var tileButtonVisual = tileButton.Q<VisualElement>(className: "tile-colour-visual");
-            tileButtonVisual.style.display = DisplayStyle.Flex;
-        }
-        else if (previouslyOpenedTile != null)
-        {
-            // open the tile
-            var tileButtonVisual = tileButton.Q<VisualElement>(className: "tile-colour-visual");
-            tileButtonVisual.style.display = DisplayStyle.Flex;
-
-            // check for a match
-            string previousTileColourString = previouslyOpenedTile.Q<VisualElement>(name: "TileColour").GetClasses().First();
-            string currentTileColourString = tileButton.Q<VisualElement>(name: "TileColour").GetClasses().First();
-
-            if (previousTileColourString == currentTileColourString)
+            if (!isOpeningAllowed)
             {
-                // to temporarily disallow opening other tiles
-                isOpeningAllowed = false;
-
-                this.StartCoroutine(MatchFoundCoroutine(previouslyOpenedTile, tileButton));
+                return;
             }
-            else
+
+            if (previouslyOpenedTile == tileButton)
             {
-                isOpeningAllowed = false;
-                this.StartCoroutine(NotAMatchCoroutine(previouslyOpenedTile, tileButton));
+                // close the tile
+                var tileButtonVisual = tileButton.Q<VisualElement>(className: "tile-colour-visual");
+                tileButtonVisual.style.display = DisplayStyle.None;
+                previouslyOpenedTile = null;
             }
+            else if (previouslyOpenedTile == null)
+            {
+                previouslyOpenedTile = tileButton;
 
-            previouslyOpenedTile = null;
+                // open the tile
+                var tileButtonVisual = tileButton.Q<VisualElement>(className: "tile-colour-visual");
+                tileButtonVisual.style.display = DisplayStyle.Flex;
+            }
+            else if (previouslyOpenedTile != null)
+            {
+                // open the tile
+                var tileButtonVisual = tileButton.Q<VisualElement>(className: "tile-colour-visual");
+                tileButtonVisual.style.display = DisplayStyle.Flex;
+
+                // check for a match
+                string previousTileColourString = previouslyOpenedTile.Q<VisualElement>(name: "TileColour").GetClasses().First();
+                string currentTileColourString = tileButton.Q<VisualElement>(name: "TileColour").GetClasses().First();
+
+                if (previousTileColourString == currentTileColourString)
+                {
+                    // to temporarily disallow opening other tiles
+                    isOpeningAllowed = false;
+
+                    this.StartCoroutine(MatchFoundCoroutine(previouslyOpenedTile, tileButton));
+                }
+                else
+                {
+                    isOpeningAllowed = false;
+                    this.StartCoroutine(NotAMatchCoroutine(previouslyOpenedTile, tileButton));
+                }
+
+                previouslyOpenedTile = null;
+            }
         }
-    }
 
-    private IEnumerator MatchFoundCoroutine(Button first, Button second)
-    {
-        // give the player 1 second of time to look
-        yield return new EditorWaitForSeconds(1);
-
-        // then we mark both tiles as matched
-        first.Q<VisualElement>(className: "tile-colour-visual").style.display = DisplayStyle.None;
-        second.Q<VisualElement>(className: "tile-colour-visual").style.display = DisplayStyle.None;
-
-        first.text = "X";
-        second.text = "X";
-
-        first.SetEnabled(false);
-        second.SetEnabled(false);
-
-        // we now have 2 less tiles to match
-        unmatchedTilesLeft -= 2;
-
-        // and we allow opening of tiles
-        isOpeningAllowed = true;
-
-        if (unmatchedTilesLeft <= 0)
+        private IEnumerator MatchFoundCoroutine(Button first, Button second)
         {
-            titleContent = new GUIContent("Memory Tile - YOU WIN");
+            // give the player 1 second of time to look
+            yield return new EditorWaitForSeconds(1);
+
+            // then we mark both tiles as matched
+            first.Q<VisualElement>(className: "tile-colour-visual").style.display = DisplayStyle.None;
+            second.Q<VisualElement>(className: "tile-colour-visual").style.display = DisplayStyle.None;
+
+            first.text = "X";
+            second.text = "X";
+
+            first.SetEnabled(false);
+            second.SetEnabled(false);
+
+            // we now have 2 less tiles to match
+            unmatchedTilesLeft -= 2;
+
+            // and we allow opening of tiles
+            isOpeningAllowed = true;
+
+            if (unmatchedTilesLeft <= 0)
+            {
+                titleContent = new GUIContent("Memory Tile - YOU WIN");
+            }
         }
-    }
 
-    private IEnumerator NotAMatchCoroutine(Button first, Button second)
-    {
-        // give the player 1 second of time to look
-        yield return new EditorWaitForSeconds(1);
+        private IEnumerator NotAMatchCoroutine(Button first, Button second)
+        {
+            // give the player 1 second of time to look
+            yield return new EditorWaitForSeconds(1);
 
-        // then we cover those tiles back
-        first.Q<VisualElement>(className: "tile-colour-visual").style.display = DisplayStyle.None;
-        second.Q<VisualElement>(className: "tile-colour-visual").style.display = DisplayStyle.None;
+            // then we cover those tiles back
+            first.Q<VisualElement>(className: "tile-colour-visual").style.display = DisplayStyle.None;
+            second.Q<VisualElement>(className: "tile-colour-visual").style.display = DisplayStyle.None;
 
-        // and we allow opening of tiles
-        isOpeningAllowed = true;
+            // and we allow opening of tiles
+            isOpeningAllowed = true;
+        }
     }
 }
